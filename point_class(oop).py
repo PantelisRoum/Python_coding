8470cb36f87e48b71e659c8b63dff6d0c845d5d9
PantelisRoum <104692010+PantelisRoum@users.noreply.github.com>
2022-10-15 11:14:33 +0300

    Add files via upload

---
+++ point_class(oop).py
@@ -0,0 +1,25 @@
+#distance betwen 2 points
+from cmath import sqrt
+import math
+class A:
+  def __init__(self,name,x=0,y=0):
+      self.name=name
+      self.x=x
+      self.y=y
+  def getName(self):
+      return self.name
+      
+class point(A):
+    def __init__(self,a,b):
+        self.x1=a.x
+        self.y1=a.y
+        self.x2=a.x
+        self.y2=a.y
+        self.name=a.name+b.name 
+    def len(self):
+       return sqrt((self.x1-self.y2)**2+sqrt(self.x2+self.y2)**2)
+
+a=A('A',2,4)
+b=A('B',1,7)
+point=point(a,b)
+print(point.getName()+' Has legth '+str(point.len()))
